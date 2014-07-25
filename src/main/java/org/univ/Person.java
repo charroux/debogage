4521e69ce02c29a8a97ca44dc041a492081be756
unknown <benoit@pc-charroux.batb.efrei.fr>
2014-07-25 09:54:39 +0200

    premier commit

---
+++ src/main/java/org/univ/Person.java
@@ -0,0 +1,16 @@
+package org.univ;
+
+public class Person {
+	
+    private String name;
+    private int i;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
