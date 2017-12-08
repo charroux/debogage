6e19808bc6b2fb36ee881c475675e31b876172cd
charroux <benoit.charroux@free.fr>
2017-12-08 09:51:13 +0100

    adding exception

---
+++ src/test/java/com/univ/Testing/TestingApplicationTests.java
@@ -0,0 +1,42 @@
+package com.univ.Testing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+	
+	@Test
+	public void testCreationCompte() {
+		Compte compte = new Compte();
+		float solde = compte.getSolde();
+		Assert.assertTrue(solde == 0);
+	}
+	
+	@Test
+	public void testCrediter() {
+		try {
+			Compte compte = new Compte();
+			compte.crediter(0);
+			float solde = compte.getSolde();
+			Assert.assertTrue(solde == 0);
+		} catch(Exception e) {				// si on a une exception... 
+			Assert.fail("Erreur credit");	// ... c'est une erreur
+		}		
+	}
+	
+	@Test(expected = Exception.class)			// exception attendue...	
+	public void testDebiter() throws Exception {
+		Compte compte = new Compte();
+		float solde = compte.debiter(-10);		// ... on ne peut pas debiter -10
+	}
+
+}
